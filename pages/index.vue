49a12c1537c3117b1c674ad3f7f987e4da49d9b7
sbilxxxx <sotobenjamin0307@gmail.com>
2021-09-07 22:21:12 +0900

    initialised npm

--- pages/index.vue
+++ pages/index.vue
@@ -1,48 +1,29 @@
 <template>
-  <div>
-    <h1>Hello Benjamin</h1>
-    <p>this is a todo app</p>
-    <div class="addArea">
-      <input type="text" v-model="inputStr">
-      <button class="addButton" @click="doAdd">追加</button>
-      <div>番号 タスク</div>
-      <ul>
-        <li v-for="(item, index) in list" v-bind:key="item.id">
-          {{item.id}} {{item.todo}}
-          <input type="checkbox" v-model="item.state" />
-          <button @click="doRemove(index)">タスクを消去</button>
-        </li>
-      </ul>
-    </div>
+  <div class="container">
+      <button class="button is-primary is-rounded" @click="login">
+        ログイン
+      </button>
   </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        inputStr: 'タスクを入力してください',
-        // タスクインスタンス(stateは完了・未完了をboolで表現)
-        list: [
-          {id: 1, todo: "掃除", state: false},
-          {id: 2, todo: "洗濯", state: true},
-          {id: 3, todo: "炊飯", state: false},
-        ]
-      }
-    },
-    methods: {
-      doAdd: function() {
-        var max = this.list.reduce(function (a, b) {
-          return a > b.id ? a : b.id
-        }, 0);
-        this.list.push({
-          "id": max + 1,
-          "todo": this.inputStr,
-        })
-      },
-      doRemove: function(index) {
-        this.list.splice(index, 1)
-      }
+export default {
+  methods: {
+    login() {
+      console.log('login')
+      // this.$store.dispatch('login')
     }
-  };
+  }
+}
 </script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
